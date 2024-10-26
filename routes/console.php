82fe379e234cf4ba2a92f1b8ffc39dd145b092e4
Knut Ola Topland <knutola@topland.net>
2024-10-26 19:31:00 +0900

    update

--- routes/console.php
+++ routes/console.php
@@ -1,1 +1,6 @@
 <?php
+
+use Illuminate\Support\Facades\Schedule;
+
+Schedule::command('employee-cvs:delete-old')->dailyAt('00:00');
+Schedule::command('queue:work --tries=3 --stop-when-empty')->everyMinute()->withoutOverlapping()->runInBackground();
