4614999ec5a466d0aee1966c290289fb5475e3a8
Knut Ola Topland <knutola@topland.net>
2024-11-29 08:13:31 +0900

    update

--- database/migrations/2024_09_07_123429_create_employee_cvs_table.php
+++ database/migrations/2024_09_07_123429_create_employee_cvs_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('employee_cvs', function (Blueprint $table) {
-            $table->uuid('id');
+            $table->uuid('id')->primary();
             $table->string('job_title')->nullable();
             $table->date('work_start_date')->nullable();
             $table->date('birth_date')->nullable();
