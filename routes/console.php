54df4ca47c31e3306b09a6f7a7cd53e3eb26276b
Knut Ola Topland <knutola@topland.net>
2025-05-15 22:11:37 +0900

    update

--- routes/console.php
+++ routes/console.php
@@ -5,7 +5,11 @@ use Illuminate\Support\Facades\Schedule;
 Schedule::command('employee-cvs:delete-old-records')->dailyAt('00:00');
 Schedule::command('employee-cvs:delete-emtpy-records')->dailyAt('08:00');
 // Schedule::command('queue:work --timeout=180 --tries=3 --stop-when-empty')->everyMinute()->withoutOverlapping()->runInBackground();
-Schedule::command('queue:work --tries=3 --stop-when-empty')->everyMinute()->withoutOverlapping()->runInBackground();
+// This is still not the recommended way to run a persistent queue worker
+Artisan::command('run-queue-worker', function () {
+    $this->info('Starting queue worker...');
+    $this->call('queue:work', ['--tries' => 3, '--stop-when-empty' => true]);
+})->everyMinute()->withoutOverlapping()->runInBackground();
 // Schedule::command("queue:work --once --name=default --queue=default --backoff=0 --memory=128 --sleep=3 --tries=1")
 //     ->everyMinute()
 //     ->withoutOverlapping(10)
