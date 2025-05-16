2d3b3536665af6a55ca74bd9ca79f3d975b8bef2
Knut Ola Topland <knutola@topland.net>
2025-05-16 19:23:55 +0900

    update

--- routes/console.php
+++ routes/console.php
@@ -6,12 +6,13 @@ Schedule::command('employee-cvs:delete-old-records')->dailyAt('00:00');
 Schedule::command('employee-cvs:delete-emtpy-records')->dailyAt('08:00');
 // Schedule::command('queue:work --timeout=180 --tries=3 --stop-when-empty')->everyMinute()->withoutOverlapping()->runInBackground();
 Schedule::command('queue:work --tries=3 --stop-when-empty')->everyMinute()->withoutOverlapping()->runInBackground()
-    ->before(function () {
-        Log::info('Scheduled task [queue:work] is starting...');
-    })
-    ->then(function () {
-        Log::info('Scheduled task [queue:work] finished successfully.');
-    });;
+    // ->before(function () {
+    //     Log::info('Scheduled task [queue:work] is starting...');
+    // })
+    // ->then(function () {
+    //     Log::info('Scheduled task [queue:work] finished successfully.');
+    // });
+;
 // Schedule::command("queue:work --once --name=default --queue=default --backoff=0 --memory=128 --sleep=3 --tries=1")
 //     ->everyMinute()
 //     ->withoutOverlapping(10)
