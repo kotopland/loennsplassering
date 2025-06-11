fb03d346a4edbd3b5c0eabfd0287b0f649396395
Knut Ola Topland <knutola@topland.net>
2025-06-11 22:23:21 +0900

    update

--- routes/console.php
+++ routes/console.php
@@ -4,3 +4,4 @@ use Illuminate\Support\Facades\Schedule;
 
 Schedule::command('employee-cvs:delete-old-records')->dailyAt('00:00');
 Schedule::command('employee-cvs:delete-emtpy-records')->dailyAt('08:00');
+Schedule::command('backup:run')->dailyAt('23:30');
