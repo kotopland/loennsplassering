ee62f1d980b4a85a3c65e25a0c17579eb561ef51
Knut Ola Topland <knutola@topland.net>
2025-06-12 13:31:00 +0900

    update

--- routes/console.php
+++ routes/console.php
@@ -4,4 +4,4 @@ use Illuminate\Support\Facades\Schedule;
 
 Schedule::command('employee-cvs:delete-old-records')->dailyAt('00:00')->timezone('Europe/Oslo');;
 Schedule::command('employee-cvs:delete-emtpy-records')->dailyAt('08:00')->timezone('Europe/Oslo');;
-Schedule::command('backup:run')->dailyAt('06:25')->timezone('Europe/Oslo');
+Schedule::command('backup:run')->dailyAt('06:32')->timezone('Europe/Oslo');
