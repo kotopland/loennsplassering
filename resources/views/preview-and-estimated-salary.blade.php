b54ff485604dc0da287763deaf91fb823d68c804
Knut Ola Topland <knutola@topland.net>
2025-10-08 22:40:16 +0900

    text update

--- resources/views/preview-and-estimated-salary.blade.php
+++ resources/views/preview-and-estimated-salary.blade.php
@@ -56,7 +56,7 @@
             <h3>Se lønnsplasseringen her:</h3>
             <div class="my-4">
 
-                <a href="#" class="btn btn-sm btn-success" _="on click remove .d-none from #beregning then wait 200ms then go to #beregning @if (!$application->email_sent) then confirm('Ikke glem å motta beregningen og lenken til dette skjemaet via e-post') @endif">
+                <a href="#" class="btn btn-sm btn-success" _="on click remove .d-none from #beregning then wait 200ms then go to #beregning @if (!$application->email_sent) then confirm('Ikke glem å trykke på knappen \'Send inn til Frikirkens hovedkontor for behandling\' etter at du har sett på den beregnede plasseringen') @endif">
                     <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 32 32">
                         <path fill="currentColor" d="M11 19v2H5v-2h2v-5H5v-2h2v-1h2v8zm8 0h-4v-2h2c1.103 0 2-.897 2-2v-2c0-1.103-.897-2-2-2h-4v2h4v2h-2c-1.103 0-2 .897-2 2v4h6zm6-8h-4v2h4v2h-3v2h3v2h-4v2h4c1.103 0 2-.897 2-2v-6c0-1.103-.897-2-2-2M2 4v4h2V4h4V2H4a2 2 0 0 0-2 2m26-2h-4v2h4v4h2V4a2 2 0 0 0-2-2M4 28v-4H2v4a2 2 0 0 0 2 2h4v-2zm24-4v4h-4v2h4a2 2 0 0 0 2-2v-4z">
                         </path>
@@ -65,7 +65,7 @@
                 </a>
             </div>
             <div class="my-4">
-                <a href="#" class="btn btn-sm btn-success" _="on click remove .d-none from #tidslinje then wait 200ms then go to #tidslinje  @if (!$application->email_sent) then confirm('Ikke glem å motta beregningen og lenken til dette skjemaet via e-post') @endif">
+                <a href="#" class="btn btn-sm btn-success" _="on click remove .d-none from #tidslinje then wait 200ms then go to #tidslinje  @if (!$application->email_sent) then confirm('Ikke glem å trykke på knappen \'Send inn til Frikirkens hovedkontor for behandling\' etter at du har sett på den beregnede plasseringen') @endif">
                     <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 2048 2048">
                         <path fill="currentColor" d="M256 640h640v128H256zm1024 512h512v128h-512zm768-896v1408H0V256zm-127 128h-514v384h-127V384H768v128H640V384H128v1153h512V896h128v641h512v-129h127v129h514zM897 896h639v128H897z">
                         </path>
